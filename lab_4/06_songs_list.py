3af660fefca94d44a2d8ff67d24675fb731444ba
Kasmynin <kasmynin06@gmail.com>
2024-11-11 19:29:30 +0500

    Update 06_songs_list.py

--- lab_4/06_songs_list.py
+++ lab_4/06_songs_list.py
@@ -42,3 +42,22 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+```
+violator_songs_list = [
+    ['World in My Eyes', 4.86],
+    ['Sweetest Perfection', 4.43],
+    ['Personal Jesus', 4.56],
+    ['Halo', 4.9],
+    ['Waiting for the Night', 6.07],
+    ['Enjoy the Silence', 4.20],
+    ['Policy of Truth', 4.76],
+    ['Blue Dress', 4.29],
+    ['Clean', 5.83],
+]
+
+total_time = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1]
+print('Три песни звучат', round(total_time, 2), 'минут')
+
+total_time = violator_songs_list[1][1] + violator_songs_list[6][1] + violator_songs_list[7][1]
+print('А другие три песни звучат', round(total_time, 2), 'минут')
+```
