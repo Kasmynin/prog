92f62b836906e4f9fb9e5da702630430636d6967
Kasmynin <kasmynin06@gmail.com>
2024-11-11 11:19:27 +0500

    Update 00_distance.py

--- lab_4/00_distance.py
+++ lab_4/00_distance.py
@@ -17,7 +17,26 @@ distances = {}
 # TODO здесь заполнение словаря
 
 print(distances)
+```py
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
 
+distances = {}
+
+for city1 in sites:
+    distances[city1] = {}
+    for city2 in sites:
+        if city1 != city2:
+            x1, y1 = sites[city1]
+            x2, y2 = sites[city2]
+            distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+            distances[city1][city2] = distance
+
+print(distances)
+```
 
 
 
