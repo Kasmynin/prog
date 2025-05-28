a27b343866d35d1fc6bb1a273e73efc61028f4bc
Kasmynin <kasmynin06@gmail.com>
2025-05-28 19:17:21 +0500

    Update request.py

--- lab17/request.py
+++ lab17/request.py
@@ -1,21 +1,20 @@
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
-from models import Client, Tour, Country, client_tour, Base # Base нужно для запросов
+from models import Client, Tour, Country, client_tour, Base 
 from tabulate import tabulate
 
-# Database engine setup
-engine = create_engine('sqlite:///travel.db', echo=True) # Use the same database file as models.py
+engine = create_engine('sqlite:///travel.db', echo=True) 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# 1. Get all clients
+# 1. Найти всех клиентов
 clients = session.query(Client).all()
 print("\n--- Список всех клиентов ---")
 data = [[client.name, client.email, client.phone_number] for client in clients]
 headers = ["Имя", "Email", "Телефон"]
 print(tabulate(data, headers=headers, tablefmt="grid"))
 
-# 2. Get all tours to France
+# 2. Найти все туры во Францию
 france = session.query(Country).filter_by(country_name="France").first()
 if france:
     tours_to_france = france.tours
@@ -26,7 +25,7 @@ if france:
 else:
     print("\nНет туров во Францию")
 
-# 3. Get all clients who booked the "Italian Adventure" tour
+# 3. Найти всех клиентов, которые забронировали тур "Итальянское приключение"
 italian_adventure = session.query(Tour).filter_by(tour_name="Italian Adventure").first()
 if italian_adventure:
     clients_on_italian_adventure = italian_adventure.clients
@@ -37,14 +36,14 @@ if italian_adventure:
 else:
     print("\nТур Italian Adventure не найден")
 
-# 4. Find tours that cost more than $1500
+# 4. Найти туры стоимостью более 1500 долларов
 expensive_tours = session.query(Tour).filter(Tour.price > 1500).all()
 print("\n--- Туры дороже $1500 ---")
 data = [[tour.tour_name, tour.price] for tour in expensive_tours]
 headers = ["Название тура", "Цена"]
 print(tabulate(data, headers=headers, tablefmt="grid"))
 
-# 5. Fixed query for tour counts per client
+# 5. Количества туров на одного клиента
 tour_counts = session.query(
     Client.name,
     func.count(client_tour.c.tour_id)
